42f426f2d07b44a4ab6fabd356218954ae2358f2
Victor Andre de Paula e Silva <victorandre.ps@gmail.com>
2022-05-26 15:42:01 +1000

    Initial Commit

---
+++ MessageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageApp
+//
+//  Created by Victor Andre de Paula e Silva on 26/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
